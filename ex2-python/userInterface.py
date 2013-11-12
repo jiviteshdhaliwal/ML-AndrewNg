26a1271eeafc940ca586263c65d6ec56c8f31fbe
= <=>
2013-11-12 23:14:01 +0530

    Removed unnecessary comments and functions that were no longer being used

--- ex2-python/userInterface.py
+++ ex2-python/userInterface.py
@@ -20,8 +20,6 @@ import pandas as pd
 ###################
 
 from computeCost import *
-from performNormalization import *
-from fmin import *
 
 def __main__():
     '''Perform Logistic Regression using gradient descent or normal equations on data provided by user'''
@@ -41,52 +39,22 @@ def __main__():
 
     initial_theta = zeros(n + 1)
 
-
-    result_Newton_CG= optimize.minimize(lambda t: cost(t, X_array, y_array),
+    try:
+        result_Newton_CG= optimize.minimize(lambda t: cost(t, X_array, y_array),
                                 initial_theta, method='Newton-CG', jac=True)
+                                # In this optimization, jac = True indicates that the function 
+                                # in this case 'cost' returns the jacobian/ gradient as well
 
+    except:
+        print 'Unable to perform Newton-CG optimization on data'
+        exit(1)
 
-#    dataFileName = raw_input('Please enter the filename of data file: ')
-#    dataDelimiter    = raw_input('Please enter the delimiter : ')
-#    
-#    try:
-#        data = genfromtxt(open(dataFileName, 'r') ,delimiter = dataDelimiter, comments= '""') # Directly imports to 'data' 
-#        X = data[:,0:-1]                            # Assuming that the last row of data is the target value
-#                                                    # and all others are those of X
-#        y = data[:,[-1]]                            # Note the single parenthesis to assert that we are choosing
-#                                                    # one single column
-##        if shape(X)[1] <= 2:
-##            plotdata(X,y)                           # Display data to user if possible
-#
-#        ################################
-#        # Perform Normalization
-#        ################################
-#
-#        X = hstack((ones((shape(X)[0], 1)), X))                  # Add X0 = 1 
-#
-#    except:
-#        print 'Unable to process data'
-#        exit(1)                                     # Exit the program with error code 1
-
-    ###############################
-    # Assign Theta, alpha, num_iter
-    ###############################  
-    
-#    initialTheta = zeros((shape(X)[1],1))              # The number of columns of X is the no. of parameters
-  
-#    print 'Cost and gradient at initial theta: ', cost(initialTheta, X, y)
- 
+    print 'Optimization performed on function'
     raw_input('Paused ')
 
     ###########################
     # Perform Machine Learning
     ###########################
-
-    # Using optimization function Without using gradient 
-
-#    theta = optimize.minimize(lambda t: cost(t, X, y), initialTheta, method ='Newton-CG' , jac = True).x
-
- #   raw_input('Paused')
 
     theta = result_Newton_CG.x
     print 'Theta:', theta
@@ -96,12 +64,6 @@ def __main__():
     p = predict(theta,X_array)
 
     print 'Training accuracy: ', float(mean(p == y) * 100)
-
-    # Using optimization function using gradient 
-
-#    theta = optimize.fmin_bfgs(cost, x0 = initialTheta, fprime = gradDes, args = (X, y)) 
-    
-#    print 'Theta:', theta 
 
     exit(0)
 
